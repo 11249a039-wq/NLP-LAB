a122c3f818cfd3427e7b3293f2a6b0eca42b8489
11249a039-wq <11249a039@kanchiuniv.ac.in>
2026-09-10 15:06:46 +0530

    Add files via upload

---
+++ labexp3.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+text = input("Enter a sentence: ")
+words = word_tokenize(text)
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+print("\nMorphological Analysis")
+print("-" * 60)
+print("{:<15} {:<15} {:<15}".format("Original", "Stemmed", "Lemmatized"))
+print("-" * 60)
+for word in words:
+  stem = stemmer.stem(word)
+  lemma = lemmatizer.lemmatize(word)
+  print("{:<15} {:<15} {:<15}".format(word, stem, lemma))
